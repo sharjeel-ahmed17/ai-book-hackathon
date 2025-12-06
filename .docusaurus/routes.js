3d58c1a7d4d03cfeb39e71e4d636a97870aa5d05
codeverseinnovation <142432342+sharjeel-ahmed17@users.noreply.github.com>
2025-12-07 01:45:17 +0500

    aaa

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,120 +3,120 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/ai-book-hackathon/__docusaurus/debug',
-    component: ComponentCreator('/ai-book-hackathon/__docusaurus/debug', '54a'),
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
     exact: true
   },
   {
-    path: '/ai-book-hackathon/__docusaurus/debug/config',
-    component: ComponentCreator('/ai-book-hackathon/__docusaurus/debug/config', '2b7'),
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
     exact: true
   },
   {
-    path: '/ai-book-hackathon/__docusaurus/debug/content',
-    component: ComponentCreator('/ai-book-hackathon/__docusaurus/debug/content', '162'),
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
     exact: true
   },
   {
-    path: '/ai-book-hackathon/__docusaurus/debug/globalData',
-    component: ComponentCreator('/ai-book-hackathon/__docusaurus/debug/globalData', '714'),
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
     exact: true
   },
   {
-    path: '/ai-book-hackathon/__docusaurus/debug/metadata',
-    component: ComponentCreator('/ai-book-hackathon/__docusaurus/debug/metadata', 'ec9'),
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
     exact: true
   },
   {
-    path: '/ai-book-hackathon/__docusaurus/debug/registry',
-    component: ComponentCreator('/ai-book-hackathon/__docusaurus/debug/registry', '4fc'),
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
     exact: true
   },
   {
-    path: '/ai-book-hackathon/__docusaurus/debug/routes',
-    component: ComponentCreator('/ai-book-hackathon/__docusaurus/debug/routes', '60e'),
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
     exact: true
   },
   {
-    path: '/ai-book-hackathon/docs',
-    component: ComponentCreator('/ai-book-hackathon/docs', '9bc'),
+    path: '/docs',
+    component: ComponentCreator('/docs', '287'),
     routes: [
       {
-        path: '/ai-book-hackathon/docs',
-        component: ComponentCreator('/ai-book-hackathon/docs', 'b21'),
+        path: '/docs',
+        component: ComponentCreator('/docs', '928'),
         routes: [
           {
-            path: '/ai-book-hackathon/docs',
-            component: ComponentCreator('/ai-book-hackathon/docs', 'e1b'),
+            path: '/docs',
+            component: ComponentCreator('/docs', 'dd1'),
             routes: [
               {
-                path: '/ai-book-hackathon/docs/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/intro', '642'),
+                path: '/docs/intro',
+                component: ComponentCreator('/docs/intro', '853'),
                 exact: true
               },
               {
-                path: '/ai-book-hackathon/docs/setup-guides/cloud-native/setup',
-                component: ComponentCreator('/ai-book-hackathon/docs/setup-guides/cloud-native/setup', 'c8d'),
+                path: '/docs/setup-guides/cloud-native/setup',
+                component: ComponentCreator('/docs/setup-guides/cloud-native/setup', 'a6e'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/setup-guides/digital-twin-workstation/setup',
-                component: ComponentCreator('/ai-book-hackathon/docs/setup-guides/digital-twin-workstation/setup', '063'),
+                path: '/docs/setup-guides/digital-twin-workstation/setup',
+                component: ComponentCreator('/docs/setup-guides/digital-twin-workstation/setup', '229'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/setup-guides/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/setup-guides/intro', 'b9f'),
+                path: '/docs/setup-guides/intro',
+                component: ComponentCreator('/docs/setup-guides/intro', 'df0'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/setup-guides/physical-ai-edge-kit/setup',
-                component: ComponentCreator('/ai-book-hackathon/docs/setup-guides/physical-ai-edge-kit/setup', 'b4e'),
+                path: '/docs/setup-guides/physical-ai-edge-kit/setup',
+                component: ComponentCreator('/docs/setup-guides/physical-ai-edge-kit/setup', 'c39'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/textbook/capstone/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/textbook/capstone/intro', 'a72'),
+                path: '/docs/textbook/capstone/intro',
+                component: ComponentCreator('/docs/textbook/capstone/intro', 'd62'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/textbook/digital-twin/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/textbook/digital-twin/intro', '36d'),
+                path: '/docs/textbook/digital-twin/intro',
+                component: ComponentCreator('/docs/textbook/digital-twin/intro', 'd0b'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/textbook/foundations/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/textbook/foundations/intro', '878'),
+                path: '/docs/textbook/foundations/intro',
+                component: ComponentCreator('/docs/textbook/foundations/intro', 'cbe'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/textbook/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/textbook/intro', '6e5'),
+                path: '/docs/textbook/intro',
+                component: ComponentCreator('/docs/textbook/intro', 'a48'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/textbook/nvidia-isaac/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/textbook/nvidia-isaac/intro', 'dfd'),
+                path: '/docs/textbook/nvidia-isaac/intro',
+                component: ComponentCreator('/docs/textbook/nvidia-isaac/intro', '70b'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/textbook/ros2/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/textbook/ros2/intro', '963'),
+                path: '/docs/textbook/ros2/intro',
+                component: ComponentCreator('/docs/textbook/ros2/intro', '47d'),
                 exact: true,
                 sidebar: "textbookSidebar"
               },
               {
-                path: '/ai-book-hackathon/docs/textbook/vla-humanoid/intro',
-                component: ComponentCreator('/ai-book-hackathon/docs/textbook/vla-humanoid/intro', '108'),
+                path: '/docs/textbook/vla-humanoid/intro',
+                component: ComponentCreator('/docs/textbook/vla-humanoid/intro', '3e0'),
                 exact: true,
                 sidebar: "textbookSidebar"
               }
